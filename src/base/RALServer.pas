b92316ecfc8b9916cd7a0b1bf454d85b159dd509
Mobius One <mobius1qwe@yahoo.com.br>
2023-06-26 17:36:43 -0300

    + Definições de ClientInfo e outras propriedades do servidor

--- src/base/RALServer.pas
+++ src/base/RALServer.pas
@@ -7,6 +7,20 @@ uses
   RALAuthentication, RALRoutes, RALTypes;
 
 type
+  TRALClientInfo = class
+  private
+    FMACAddress: StringRAL;
+    FIP: StringRAL;
+    FUserAgent: StringRAL;
+    procedure SetIP(const Value: StringRAL);
+    procedure SetMACAddress(const Value: StringRAL);
+    procedure SetUserAgent(const Value: StringRAL);
+  public
+    property IP: StringRAL read FIP write SetIP;
+    property MACAddress: StringRAL read FMACAddress write SetMACAddress;
+    property UserAgent: StringRAL read FUserAgent write SetUserAgent;
+  end;
+
   TRALRequest = class
   private
     FHeaders: TStringList;
@@ -14,13 +28,17 @@ type
     FContentType: StringRAL;
     FContentSize: IntegerRAL;
     FQuery: StringRAL;
+    FParams: TStringList;
+    FClientInfo: TRALClientInfo;
     procedure SetBody(const Value: TStream);
     procedure SetHeaders(const Value: TStringList);
     procedure SetContentSize(const Value: IntegerRAL);
     procedure SetContentType(const Value: StringRAL);
     procedure SetQuery(const Value: StringRAL);
+    procedure SetClientInfo(const Value: TRALClientInfo);
   public
     property Body: TStream read FBody write SetBody;
+    property ClientInfo: TRALClientInfo read FClientInfo write SetClientInfo;
     property ContentSize: IntegerRAL read FContentSize write SetContentSize;
     property ContentType: StringRAL read FContentType write SetContentType;
     property Headers: TStringList read FHeaders write SetHeaders;
@@ -32,13 +50,20 @@ type
     FHeaders: TStringList;
     FBody: TStream;
     FContentType: StringRAL;
+    FContentSize: IntegerRAL;
+    FContent: StringRAL;
+    FRawContent: TStream;
     procedure SetBody(const Value: TStream);
     procedure SetContentType(const Value: StringRAL);
     procedure SetHeaders(const Value: TStringList);
+    procedure SetContent(const Value: StringRAL);
+    procedure SetRawContent(const Value: TStream);
   public
     property Body: TStream read FBody write SetBody;
+    property Content: StringRAL read FContent write SetContent;
     property ContentType: StringRAL read FContentType write SetContentType;
     property Headers: TStringList read FHeaders write SetHeaders;
+    property RawContent: TStream read FRawContent write SetRawContent;
   end;
 
   TRALServer = class(TComponent)
@@ -56,6 +81,8 @@ type
       write FAuthentication;
     property Port: IntegerRAL read FPort write FPort;
     property Routes: TRALRoutes read FRoutes write FRoutes;
+    property Request: TRALRequest read FRequest write FRequest;
+    property Response: TRALResponse read FResponse write FResponse;
   end;
 
 implementation
@@ -87,6 +114,11 @@ begin
   FBody := Value;
 end;
 
+procedure TRALRequest.SetClientInfo(const Value: TRALClientInfo);
+begin
+  FClientInfo := Value;
+end;
+
 procedure TRALRequest.SetContentSize(const Value: IntegerRAL);
 begin
   FContentSize := Value;
@@ -114,6 +146,11 @@ begin
   FBody := Value;
 end;
 
+procedure TRALResponse.SetContent(const Value: StringRAL);
+begin
+  FContent := Value;
+end;
+
 procedure TRALResponse.SetContentType(const Value: StringRAL);
 begin
   FContentType := Value;
@@ -122,6 +159,28 @@ end;
 procedure TRALResponse.SetHeaders(const Value: TStringList);
 begin
   FHeaders := Value;
+end;
+
+procedure TRALResponse.SetRawContent(const Value: TStream);
+begin
+  FRawContent := Value;
+end;
+
+{ TRALClientInfo }
+
+procedure TRALClientInfo.SetIP(const Value: StringRAL);
+begin
+  FIP := Value;
+end;
+
+procedure TRALClientInfo.SetMACAddress(const Value: StringRAL);
+begin
+  FMACAddress := Value;
+end;
+
+procedure TRALClientInfo.SetUserAgent(const Value: StringRAL);
+begin
+  FUserAgent := Value;
 end;
 
 end.
