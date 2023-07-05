f558ff1260f735259fa50c9d2df399210103d501
Mobius One <mobius1qwe@yahoo.com.br>
2023-07-05 19:47:06 -0300

    + Adição de ícones dos componentes  + Adição de script de geração dos ícones  * Ajustes de descrição dos pacotes  * Ajustes de plataformas disponíveis pro motor Indy

---
+++ Assets/src/_gerardcr.bat
@@ -0,0 +1,7 @@
+@echo off
+set path_brcc32="D:\IDE\Embarcadero\Studio\7\Bin\brcc32.exe"
+
+# Motores
+%path_brcc32% -fo "..\..\pkg\Delphi\IndyRAL.dcr" "Indy.rc"
+%path_brcc32% -fo "..\..\pkg\Delphi\NetHttpRAL.dcr" "NetHttp.rc"
+%path_brcc32% -fo "..\..\pkg\Delphi\SynopseRAL.dcr" "Synopse.rc"
