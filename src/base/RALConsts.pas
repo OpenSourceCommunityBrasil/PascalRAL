09c3f714b64f254c97f8ee11c3c9bd66cd7fb058
Mobius One <mobius1qwe@gmail.com>
2024-11-01 16:02:45 -0300

    - Atualização de Submódulo ZSTD

--- src/base/RALConsts.pas
+++ src/base/RALConsts.pas
@@ -13,7 +13,7 @@ type
 
 const
   // Versionamento
-  RALVERSION = '0.9.10-4 alpha';
+  RALVERSION = '0.9.10-6 alpha';
   // IOTA Constants
   RALPACKAGENAME           = 'Pascal REST API Lite (RAL) Components';
   RALPACKAGESHORT          = 'PascalRAL';
