3afe925b352e82c55ef67b0cffdd5a0313860f3d
Mobius One <mobius1qwe@gmail.com>
2024-11-06 19:23:10 -0300

    - Ajuste de versionamento

--- src/base/RALConsts.pas
+++ src/base/RALConsts.pas
@@ -13,7 +13,7 @@ type
 
 const
   // Versionamento
-  RALVERSION = '0.9.10-8 alpha';
+  RALVERSION = '0.9.11-2 alpha';
   // IOTA Constants
   RALPACKAGENAME           = 'Pascal REST API Lite (RAL) Components';
   RALPACKAGESHORT          = 'PascalRAL';
