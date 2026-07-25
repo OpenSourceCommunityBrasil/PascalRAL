97544cc7e268cc8d403f1afd5ece9c3d274257e7
mobius1qwe <mobius1qwe@gmail.com>
2026-07-25 11:18:57 -0300

    fix: Correção de erro de caminho de arquivo de mensagens do pacote

--- src/base/RALConsts.pas
+++ src/base/RALConsts.pas
@@ -13,7 +13,7 @@ type
 
 const
   // Versionamento
-  RALVERSION = '1.0.1';
+  RALVERSION = '1.0.1-2';
   RALVERSION_MAJOR = 1;
   RALVERSION_MINOR = 0;
   RALVERSION_PATCH = 1;
@@ -74,11 +74,11 @@ const
 
 resourcestring
   {$IF DEFINED(LANG_PTBR)}
-    {$I ..\languages\ralconsts_ptbr.inc}
+    {$I ..\base\ralconsts_ptbr.inc}
   {$ELSEIF DEFINED(LANG_ESES)}
-    {$I ..\languages\ralconsts_eses.inc}
+    {$I ..\base\ralconsts_eses.inc}
   {$ELSE}
-    {$I ..\languages\ralconsts_enus.inc}
+    {$I ..\base\ralconsts_enus.inc}
   {$IFEND}
 
 implementation
